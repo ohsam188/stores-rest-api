576e3cd0eab285ff8697e0f06b1137a91afd7bf9
Sam <sam@Sams-MacBook-Pro.local>
2018-09-03 16:35:49 -0500

    final touch3

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +7,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'sam'
 api = Api(app)
